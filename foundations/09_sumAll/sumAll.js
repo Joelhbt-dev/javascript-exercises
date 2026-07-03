52406680877697496dee065752c851f1b364d5a0
JoelR <joelbiswal@gmail.com>
2026-07-03 21:05:53 +0530

    Function sumAll sums two integers and the numbers between them it uses nested if-else block and an else block with for loop

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,34 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+
+    sum = 0;
+
+    if ((Number.isInteger(a) && a>0) && (Number.isInteger(b) && b>0)){
+
+             if(a<b){
+
+        for(i=a;i<=b;i++){
+
+            sum = sum + i;
+
+        }
+    }
+
+    else if(a>b){
+
+        for(i=a;i>=b;i--){
+
+            sum = sum + i;
+        }
+    }
+        }
+    
+
+    else {
+
+        return "ERROR";
+    }
+
+    return sum;
 
 };
 
