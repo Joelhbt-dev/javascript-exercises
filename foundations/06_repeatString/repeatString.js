f1d3b4086f9f7ccceb678b83c3d7529900581a67
JoelR <joelbiswal@gmail.com>
2026-07-03 13:53:00 +0530

    Passed the first test, implemented string repetition using for loop and if-else block

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,4 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+
+     let s = "";
+
+    for(let i=0;i<num;i++){
+
+        if(num){
+
+         s = s + string;
+    }
+
+        else if(!num || num===0) {
+            return "ERROR";
+        }
+
+    }
+
+    return s;
+
 
 };
 
