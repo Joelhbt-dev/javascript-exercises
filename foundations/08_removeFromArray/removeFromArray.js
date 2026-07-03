7cfb77e369b0b179a7046df6672a841449062980
JoelR <joelbiswal@gmail.com>
2026-07-03 20:22:34 +0530

    Using filter() all those items are selected which are not in the removal list

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...rem) {
+
+      let newArr = arr.filter((item)=> !rem.includes(item) );
+      return newArr;
 };
 
 // Do not edit below this line
