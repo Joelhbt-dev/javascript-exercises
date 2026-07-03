8058620c1fa3f258cd31116def39d0f1c4bdd17b
JoelR <joelbiswal@gmail.com>
2026-07-03 22:28:25 +0530

    This code can convert temperatures with 1 decimal place precision

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,34 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+
+  C = (F-32)*5/9;
+  
+  if(Number.isInteger(C)){
+
+    return C;
+  }
+  else{
+
+  C = parseFloat(C.toFixed(1));
+  return C;
+  }
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+
+  let F = (C * 9/5)+32;
+
+  if(Number.isInteger(F)){
+
+    return F;
+  }
+
+  else{
+
+  F = parseFloat(F.toFixed(1));
+  return F;
+  }
 };
 
 // Do not edit below this line
