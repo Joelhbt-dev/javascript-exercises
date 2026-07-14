451e3511c2113d5d8a8d804d7e75767c51bf355b
JoelR <joelbiswal@gmail.com>
2026-07-14 23:12:52 +0530

    Palindrome function implemented

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,25 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+     const newStr = str.replace(/[^a-zA-Z0-9]/g, '');      // Regex used to clean the string
+
+    const arr = newStr.split('');
+
+    let palin = '';
+
+    for(let i=arr.length-1;i>=0;i--){
+         palin = palin + arr[i];
+    }
+
+    if(palin.toLowerCase()===newStr.toLowerCase()){
+        return true;
+    }
+    
+    else{
+
+        return false;
+    }
+
+
 
 };
 
