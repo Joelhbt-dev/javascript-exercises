028837bcc5cf414e1fe2d98759a0b4e44b57e238
JoelR <joelbiswal@gmail.com>
2026-07-14 22:36:38 +0530

    Completed the exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,24 +1,37 @@
-const add = function() {
+const add = function(a,b) {
+
+  return a+b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+	
+  return a-b;
+};
+
+const sum = (...arr)=> arr.flat().reduce((sum,i)=> sum + Number(i),0);
+
+const multiply = (arr)=> arr.reduce((mul,i)=> mul*i,1);
+
+const power = function(a,b) {
+
+  return a**b;
+
 	
 };
 
-const sum = function() {
-	
-};
+const factorial = function(num) {
 
-const multiply = function() {
+  let fact=1;
 
-};
+  for(let i=num;i>=1;i--){
 
-const power = function() {
-	
-};
+    fact = fact*i;
+  }
 
-const factorial = function() {
+  return fact;
+
+
 	
 };
 
