d58a98b0a07a60e9b68d06f3674595cc7816658d
JoelR <joelbiswal@gmail.com>
2026-07-15 13:34:50 +0530

    Fibonacci sequence generated using for-loop and array

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+
+    if(num == 0){
+
+        return 0;
+    }
+
+    else if(num<0){
+
+        return "OOPS";
+    }
+
+    num = Number(num);
+
+    let arr = [];
+    arr[0]=1;
+    arr[1]=1;
+
+    for(let i=2;i<=num;i++){
+
+        arr[i] = arr[i-1] + arr[i-2];
+
+    }
+
+
+    return arr[num-1];
 
 };
 
