ef38f6e7ca7f3319c5b5772c08d5cfb9ff18424c
JoelR <joelbiswal@gmail.com>
2026-07-15 14:59:44 +0530

    Complted this exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+
+    const getAge = (person)=>{
+
+        const death = person.yearOfDeath || new Date().getFullYear();
+        return death - person.yearOfBirth;
+    };
+
+    return arr.reduce((oldestSoFar,currentPerson)=>
+    {
+
+        const oldestAge = getAge(oldestSoFar);
+        const currentAge = getAge(currentPerson);
+
+        return currentAge > oldestAge ? currentPerson : oldestSoFar;
+    })
+
+
 
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
